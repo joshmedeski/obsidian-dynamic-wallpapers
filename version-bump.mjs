3370f74de180fdaa278d2df957d2d0f36d2fbce9
Josh Medeski <josh.medeski@gmail.com>
2026-06-23 22:47:33 -0500

    Align versions

--- version-bump.mjs
+++ version-bump.mjs
@@ -2,13 +2,16 @@ import { readFileSync, writeFileSync } from 'fs';
 
 const targetVersion = process.env.npm_package_version;
 
+const manifestPath = 'public/manifest.json';
+const versionsPath = 'versions.json';
+
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
+const manifest = JSON.parse(readFileSync(manifestPath, 'utf8'));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
+writeFileSync(manifestPath, JSON.stringify(manifest, null, '\t'));
 
 // update versions.json with target version and minAppVersion from manifest.json
-const versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+const versions = JSON.parse(readFileSync(versionsPath, 'utf8'));
 versions[targetVersion] = minAppVersion;
-writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+writeFileSync(versionsPath, JSON.stringify(versions, null, '\t'));
